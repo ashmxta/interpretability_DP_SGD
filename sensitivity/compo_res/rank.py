cd0b86996231eb735701d38f72b0d06969081a41
ashmita <ashmitab04@gmail.com>
2025-07-03 18:01:57 -0400

    compo_res computations complete

--- sensitivity/compo_res/rank.py
+++ sensitivity/compo_res/rank.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-infile = "sensitivity/compo_res/compo_res1.csv"
-outfile = "sensitivity/compo_res/compo_res1_ranked.csv"
+infile = "compo_res_concat.csv"
+outfile = "compo_res_ranked.csv"
 
 df = pd.read_csv(infile)
 ranked = df.groupby("point")["Privacy cost"].sum().sort_values()
@@ -9,3 +9,4 @@ ranked.to_csv(outfile, header=["Total Privacy Cost"])
 
 print("Saved ranked result to", outfile)
 print("Lowest 10 privacy cost points:\n", ranked.head(10))
+print("Highest 10 privacy cost points:\n", ranked.tail(10))
